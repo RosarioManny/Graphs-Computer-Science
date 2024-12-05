8c73e03c40ec5923607fe6c3b8c9a842b45708f2
RosarioManny <rosario.emm47@gmail.com>
2024-12-05 12:53:09 -0500

    Node Edges

--- graph.traversal.js
+++ graph.traversal.js
@@ -16,14 +16,28 @@ class Graph {
       this.adjacencyList[edgeNode].push(node) // < this makes it an undirected list
     }
   }
+
+  // Depth first Search
+  // Breadth first Search
+
+  BFS() {
+
+  }
 }
 
 const graph = new Graph()
 graph.addNode("A")
 graph.addNode("B")
 graph.addNode("C")
-graph.addNode("V")
+graph.addNode("E")
 graph.addNode("D")
-graph.addNode("T")
+
+graph.addEdge("A", "B")
+graph.addEdge("A", "C")
+graph.addEdge("A", "E")
+graph.addEdge("B", "C")
+graph.addEdge("C", "D")
+graph.addEdge("E", "D")
+
 
 console.log(graph)
