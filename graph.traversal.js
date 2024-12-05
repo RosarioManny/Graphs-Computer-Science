68fa867c67d8785d21678250d40463fd4e2df093
RosarioManny <rosario.emm47@gmail.com>
2024-12-05 12:48:59 -0500

    Add nodes

--- graph.traversal.js
+++ graph.traversal.js
@@ -1,15 +1,29 @@
-class graph {
+class Graph {
   constructor() {
     this.adjacencyList = {}
   }
 
 
-  addnode (node) {
-
+  addNode (node) {
+    if (!this.adjacencyList[node]) {
+      this.adjacencyList[node] = []
+    }
   }
 
   addEdge(node, edgeNode) {
-
+    if (this.adjacencyList[node] && this.adjacencyList[edgeNode]) {
+      this.adjacencyList[node].push(edgeNode)
+      this.adjacencyList[edgeNode].push(node) // < this makes it an undirected list
+    }
   }
-
 }
+
+const graph = new Graph()
+graph.addNode("A")
+graph.addNode("B")
+graph.addNode("C")
+graph.addNode("V")
+graph.addNode("D")
+graph.addNode("T")
+
+console.log(graph)
